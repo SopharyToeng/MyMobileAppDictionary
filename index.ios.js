44549d8a385e28e0f8ad8d3f254f12de89bc8e5f
SopharyMSH <sopharydev@gmail.com>
2017-06-22 15:11:32 +0700

    added searchbar from react-native-element

--- index.ios.js
+++ index.ios.js
@@ -1,17 +1,42 @@
 import React, { Component } from 'react';
-import { AppRegistry,StyleSheet,Text,View,SectionList } from 'react-native';
+import { AppRegistry,StyleSheet,Text,View,FlatList,TouchableOpacity, ScrollView } from 'react-native';
+import { SearchBar } from 'react-native-elements';
 
 export default class smart9 extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <SectionList
-          sections={[
-            {title: 'D', data: ['Devin']},
-            {title: 'J', data: ['Jackson', 'James', 'Jillian', 'Jimmy', 'Joel', 'John', 'Julie']},
+      <SearchBar
+        containerStyle={{ backgroundColor: '#f0f3f4'}}
+        inputStyle={{ backgroundColor: '#fff', color: '#000'}}
+        lightTheme
+        placeholder='Type Here...' />
+        <FlatList
+          data={[
+            {key: 'a', definition: "Hello! It's me"},
+            {key: 'a,a'},
+            {key: 'a,b,c'},
+            {key: 'a.b.c'},
+            {key: 'a.m'},
+            {key: 'a/c'},
+            {key: 'abach'},
+            {key: 'aback'},
+            {key: 'abactor'},
+            {key: 'B'},
+            {key: 'B,b'},
+            {key: 'back'},
+            {key: 'bone'},
+            {key: 'banana'},
+            {key: 'baby'},
+            {key: 'boy'},
           ]}
-          renderItem={({item}) => <Text style={styles.item}>{item}</Text>}
-          renderSectionHeader={({section}) => <Text style={styles.sectionHeader}>{section.title}</Text>}
+          renderItem={({item}) =>
+            <TouchableOpacity style={styles.item}>
+              <ScrollView>
+                <Text>{item.key}</Text>
+              </ScrollView>
+            </TouchableOpacity>
+          }
         />
       </View>
     );
@@ -28,14 +53,21 @@ const styles = StyleSheet.create({
     paddingLeft: 10,
     paddingRight: 10,
     paddingBottom: 2,
-    fontSize: 14,
+    // fontSize: 14,
     fontWeight: 'bold',
     backgroundColor: 'rgba(247,247,247,1.0)',
   },
   item: {
     padding: 10,
-    fontSize: 18,
+    // fontSize: 18,
     height: 44,
+    borderColor: '#ddd',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 3 },
+    shadowOpacity: 0.3,
+    elevation: 2,
+    position: 'relative'
+
   },
 });
 
